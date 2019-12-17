2755939e99afdef52a5cbe1657c6f87222d656bf
Cristina Fernández Sanz <cristina.fernandez@with-madrid.com>
2019-12-17 12:56:55 +0100

    Deploy path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production' ? '/web-animation-technologies/gsap/vue-gsap-3/dist/' : '/'
+    baseUrl: process.env.NODE_ENV === 'production' ? '/web-animation-technologies/gsap/vue-gsap-3/' : '/'
 }
