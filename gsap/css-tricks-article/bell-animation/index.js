b14f96b37f4fe5c8d1ccd734ba0967e7e2860054
Cristina Fernández Sanz <cristina.fernandez@with-madrid.com>
2020-01-22 17:06:05 +0100

    Comments

--- gsap/css-tricks-article/bell-animation/index.js
+++ gsap/css-tricks-article/bell-animation/index.js
@@ -2,12 +2,18 @@ const bellfull = document.querySelector('.bellfull');
 
 let notificationOpen = false;
 
+/* HIDING ELEMENTS */
+
+/* Cross with 2 lines hidden */
 gsap.set('.xline1, .xline2', {
     autoAlpha: 0,
     scaleY: 0,
     transformOrigin: '50% 0%'
 })
 
+/* EVENT LISTENER CLICK */
+
+/* Play and reverse the animation when clicking in the bell */
 bellfull.addEventListener('click', () => {
     if (!notificationOpen) {
         const tl = gsap.timeline({
@@ -30,6 +36,9 @@ bellfull.addEventListener('click', () => {
     }
 })
 
+/* ANIMATIONS */
+
+/* Timeline to chain all the animations */
 let bellTL = gsap.timeline({
     paused: true,
     defaults: {
@@ -38,26 +47,41 @@ let bellTL = gsap.timeline({
     }
 });
 
+/*
+Add label 'bell' to make sure things fire off
+at a particular point in time.
+Some elements increment and decrement from the label.
+*/
 bellTL.add('bell')
+
+// To complete the circle when the top part disappears?
 bellTL.to('.rectmask', {
     scaleY: 1.5,
     transformOrigin: '50% 100%',
     ease: 'sine.in'
 }, 'bell')
+
+// Bell top part disappears
 bellTL.to('#bell', {
     scale: 0,
     autoAlpha: 0,
     transformOrigin: '50% 50%',
     ease: 'sine.in'
 }, 'bell')
+
+// Circle goes up 0.5s after
 bellTL.to('#dingle', {
     y: -80,
     ease: 'bounce'
 }, 'bell+=0.5')
+
+// Circle disappears after 1.2s
 bellTL.to('#dingle', {
     duration: 0.1,
     autoAlpha: 0,
 }, 'bell+=1.2')
+
+// Crosses appear, scale and rotate
 bellTL.to('.xline1, .xline2', {
     duration: 0.1,
     autoAlpha: 1,
